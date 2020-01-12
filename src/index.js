1c741dc16bc4bd2c2a42f8a4a475022ca52ffaf4
kouta1612 <kouta1612world69@gmail.com>
2020-01-12 16:08:35 +0900

    fix bug for winner

--- src/index.js
+++ src/index.js
@@ -82,7 +82,8 @@ class Game extends React.Component {
   render() {
     const history = this.state.history;
     const current = history[this.state.stepNumber];
-    const winner = calculateWinner(current);
+    const squares = current.squares;
+    const winner = calculateWinner(squares);
     const moves = history.map((step, move) => {
       const desc = move ? 'Go to move #' + move : 'Go to game start';
       return (
